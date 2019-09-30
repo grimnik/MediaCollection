66fc7005417a5b14d230bb8232ac1598cefcc8be
dieter vermander <vermanderdieter@hotmail.com>
2019-09-30 12:13:41 +0200

    Added some data annotations

--- MediaCollection/Models/MusicAlbumCreateViewModel.cs
+++ MediaCollection/Models/MusicAlbumCreateViewModel.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,11 @@ namespace MediaCollection.Models
 {
     public class MusicAlbumCreateViewModel
     {
+        [Required(ErrorMessage = "Je moe wel een name ingeven eh!")]
+        [DataType(DataType.Text, ErrorMessage = "Da is gin text e menneke!")]
         public string Naam { get; set; }
-       
+        [Required(ErrorMessage = "Je moe wel een artiest name ingeven eh!")]
+        [DataType(DataType.Text, ErrorMessage = "Da is gin text e menneke!")]
         public string Artist { get; set; }
         public IFormFile Cover { get; set; }
     }
--- MediaCollection/Models/SerieCreateViewModel.cs
+++ MediaCollection/Models/SerieCreateViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MediaCollection.Domain;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace MediaCollection.Models
 {
     public class SerieCreateViewModel
     {
+        [Required(ErrorMessage = "Je moe wel een name ingeven eh!")]
+        [DataType(DataType.Text, ErrorMessage ="Da is gin text e menneke!")]
         public string Naam { get; set; }
         
         public List<SeizoenCreate> Seizoen { get; set; }
@@ -25,8 +28,12 @@ namespace MediaCollection.Models
     }
     public class AfleveringCreate
     {
-        
+        [Required(ErrorMessage = "Je moe wel een name ingeven eh!")]
+        [DataType(DataType.Text, ErrorMessage = "Da is gin text e menneke!")]
+
         public string Naam { get; set; }
+        [Required(ErrorMessage = "Je moe wel een beschrieving ingeven eh!")]
+        [DataType(DataType.Text, ErrorMessage = "Da is gin text e menneke!")]
         public string Beschrijving { get; set; }
     }
 }
